74e72ff1df8e9b5074f720d975d836b41ea7df02
cbarne02 <Christopher.Barnett@tufts.edu>
2019-02-22 14:50:11 -0500

    -add AllowedGroup to admin

--- shibboleth/admin.py
+++ shibboleth/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import AllowedUser
+from .models import AllowedUser, AllowedGroup
 # Register your models here.
+
+
+class AllowedGroupInline(admin.StackedInline):
+    model = AllowedGroup
 
 
 @admin.register(AllowedUser)
@@ -11,5 +15,6 @@ class AllowedUserAdmin(admin.ModelAdmin):
         ])
     groups_display.short_description = "Groups"
 
+    inlines= [AllowedGroupInline]
     list_display = ('username', 'groups_display', 'is_superuser', 'is_staff')
     filter_horizontal = ['groups']
